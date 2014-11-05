b4f82712dda7d75bf6bdc4fc1546e35fbfbcd94b
Raevn <d_bened@raevn.com>
2014-11-05 22:10:43 +1100

    Update to v0.3.0
    Change: Displays a message stating news is awaiting approval once
    submitted
    Change: News items can now have tags
    Change: Tags that match content IDs are automatically shown as the
    content name
    Change: Tags can be clicked to filter news to only show items with that
    tag
    Change: News feed re-styled
    Change: User names are clickable to view Uber profile on the forums
    Change: News items can now be assigned a category (changes what Icon is
    displayed)
    API: pahub.api.news.addNewsItem and pahub.api.news.displayPreview now
    have news_tags & news_icon parameters
    API: pahub.api.news.setFilter(tag) added

--- com.pahub.content.plugin.community/com.pahub.content.plugin.community.js
+++ com.pahub.content.plugin.community/com.pahub.content.plugin.community.js
@@ -16,37 +16,105 @@ function load_plugin_community(data, folder) {
 	setConstant("NEWS_URL", "http://pahub.raevn.com/news.php?action=get");
 	
 	pahub.api["news"] = {
-		addNewsItem: function(news_title, news_text, news_author) { model.news.addNewsItem(news_title, news_text, news_author); },
-		displayPreview: function(news_title, news_text, news_author) { model.news.displayPreview(news_title, news_text, news_author); }
+		addNewsItem: function(news_title, news_text, news_author, news_tags, news_icon) { model.news.addNewsItem(news_title, news_text, news_author, news_tags, news_icon); },
+		displayPreview: function(news_title, news_text, news_author, news_tags, news_icon) { model.news.displayPreview(news_title, news_text, news_author, news_tags, news_icon); },
+		setFilter: function(tag) { model.news.setFilter(tag); }
 	}
 	
 	model["news"] = {
+		folder_path: ko.observable(folder),
 		news_items: ko.observableArray(),
 		news_preview: ko.observable(),
 		news_loading: ko.observable(false),
+		show_submit_message: ko.observable(false),
+		filter_value: ko.observable(""),
+		filter_text: ko.computed({
+			read: function() {
+				if (model.news.filter_value() != "") {
+					if (pahub.api.content.contentItemExists(false, model.news.filter_value()) == true) {
+						var content = pahub.api.content.getContentItem(false, model.news.filter_value());
+						return content.data.display_name;
+					}
+					if (pahub.api.content.contentItemExists(true, model.news.filter_value()) == true) {
+						var content = pahub.api.content.getContentItem(true, model.news.filter_value());
+						return content.data.display_name;
+					}
+					odel.news.filter_value("");
+				}
+				return model.news.filter_value();
+			},
+			deferEvaluation: true
+		}),
+		available_icons: ko.observableArray([
+			{
+				name: "Content",
+				url: "news-content.png"
+			},
+			{
+				name: "General",
+				url: "news-item.png"
+			},
+			{
+				name: "PA",
+				url: "news-pa.png"
+			}
+		]),
 		
 		show_add: ko.observable(false),
 		
+		setFilter: function(tag) {
+			model.news.filter_value(tag);
+		},
+		
+		filteredNews: ko.computed({
+			read: function() {
+				if (model.news.filter_value() == "") {
+					return model.news.news_items();
+				} else {
+					var filteredItems = [];
+					for (var i = 0; i < model.news.news_items().length; i++) {
+						var included = false;
+						var news_item = model.news.news_items()[i];
+						for (var j = 0; j < news_item.tags().length; j++) {
+							if (news_item.tags()[j].value == model.news.filter_value()) {
+								included = true;
+							}
+						}
+						if (included == true) {
+							filteredItems.push(news_item);
+						}
+					}
+					return filteredItems;
+				}
+			},
+			deferEvaluation: true
+		}),
+		
 		loadNews: function() {
 			model.news.news_loading(true);
+			model.news.show_submit_message(false);
 			var id = pahub.api.resource.loadResource(constant.NEWS_URL, "save", {
 				name: "PA Community News",
 				saveas: "news.json", 
 				mode: "async",
 				success: function() {
+					
 					model.news.news_loading(false);
 					model.news.news_items.removeAll();
 					var newsJSON = readJSONfromFile(path.join(constant.PAHUB_CACHE_DIR, "news.json"));
 					for (var i = 0; i < newsJSON.length; i++) {
 						newsJSON[i].text = md.render(newsJSON[i].text.replace(/\u0001/g, "."));
 						newsJSON[i].title = newsJSON[i].title.replace(/\u0001/g, ".");
+						newsJSON[i].icon = path.join(folder, newsJSON[i].icon.replace(/\u0001/g, "."));
+											
+						newsJSON[i].tags = ko.observableArray(model.news.getTagArray(newsJSON[i].tags));
 						model.news.news_items.push(newsJSON[i]);
 					}
 				}
 			});
 		},
 
-		validateNewsItem: function(news_title, news_text, news_author) {
+		validateNewsItem: function(news_title, news_text, news_author, news_tags, news_icon) {
 			if (news_title == "") {
 				return false;
 			}
@@ -56,31 +124,86 @@ function load_plugin_community(data, folder) {
 			if (news_author == "") {
 				return false;
 			}
+			if (news_icon == "") {
+				return false;
+			}
 			return true;
 		},
 		
-		addNewsItem: function(news_title, news_text, news_author) {
-			if (model.news.validateNewsItem(news_title, news_text, news_author) == true) {
-				pahub.api.resource.loadResource("http://pahub.raevn.com/news.php?action=add&title=" + encodeURIComponent(news_title).replace(/\./g, "%01") + "&author=" + encodeURIComponent(news_author).replace(/\./g, "%01") + "&text=" + encodeURIComponent(news_text).replace(/\./g, "%01"), "get", {name: "Submit News Item", mode: "async"});
-				model.news.show_add(false);
+		updateTags: function() {
+			for (var i = 0; i < model.news.news_items().length; i++) {
+				var news_item = model.news.news_items()[i];
+				for (var j = 0; j < news_item.tags().length; j++) {
+					var contentOnline = pahub.api.content.getContentItem(false, news_item.tags()[j].value);
+					var contentLocal = pahub.api.content.getContentItem(true, news_item.tags()[j].value);
+					if (pahub.api.content.contentItemExists(false, news_item.tags()[j].value) == true) {
+						news_item.tags.replace(news_item.tags()[j], {
+							name: contentOnline.data.display_name,
+							value: news_item.tags()[j].value
+						});
+					}
+					if (pahub.api.content.contentItemExists(true, news_item.tags()[j].value) == true) {
+						news_item.tags.replace(news_item.tags()[j], {
+							name: contentLocal.data.display_name,
+							value: news_item.tags()[j].value
+						});
+					}
+				}
 			}
 		},
 		
-		displayPreview: function(news_title, news_text, news_author) {
-			if (model.news.validateNewsItem(news_title, news_text, news_author) == true) {
+		getTagArray: function(news_tags) {
+			var tagValues = news_tags.split(",");
+			var tagArray = [];
+			
+			for (var i = 0; i < tagValues.length; i++) {
+				tagArray.push({
+					name: tagValues[i].trim().replace(/\u0001/g, "."),
+					value: tagValues[i].trim().replace(/\u0001/g, ".")
+				});
+				var contentOnline = pahub.api.content.getContentItem(false, tagArray[i].value.replace(/\u0001/g, "."));
+				var contentLocal = pahub.api.content.getContentItem(true, tagArray[i].value.replace(/\u0001/g, "."));
+				if (pahub.api.content.contentItemExists(false, tagArray[i].value.replace(/\u0001/g, ".")) == true) {
+					tagArray[i] = {
+						name: contentOnline.data.display_name,
+						value: tagArray[i].value.replace(/\u0001/g, ".")
+					};
+				}
+				if (pahub.api.content.contentItemExists(true, tagArray[i].value.replace(/\u0001/g, ".")) == true) {
+					tagArray[i] = {
+						name: contentLocal.data.display_name,
+						value: tagArray[i].value.replace(/\u0001/g, ".")
+					};
+				}
+			}
+			return tagArray;
+		},
+		
+		addNewsItem: function(news_title, news_text, news_author, news_tags, news_icon) {
+			if (model.news.validateNewsItem(news_title, news_text, news_author, news_tags, news_icon) == true) {
+				pahub.api.resource.loadResource("http://pahub.raevn.com/news.php?action=add&title=" + encodeURIComponent(news_title).replace(/\./g, "%01") + "&author=" + encodeURIComponent(news_author).replace(/\./g, "%01") + "&text=" + encodeURIComponent(news_text).replace(/\./g, "%01") + "&tags=" + encodeURIComponent(news_tags).replace(/\./g, "%01") + "&icon=" + encodeURIComponent(news_icon).replace(/\./g, "%01"), "get", {name: "Submit News Item", mode: "async"});
+				model.news.show_add(false);
+				model.news.show_submit_message(true);
+			}
+		},
+		
+		displayPreview: function(news_title, news_text, news_author, news_tags, news_icon) {
+			if (model.news.validateNewsItem(news_title, news_text, news_author, news_tags, news_icon) == true) {
 				news_text = md.render(news_text.replace(/\u0001/g, "."));
 				model.news.news_preview({
 					title: news_title,
 					author: news_author,
 					date: getDateTimeString(new Date()),
-					text: news_text
+					text: news_text,
+					tags: ko.observableArray(model.news.getTagArray(news_tags)),
+					icon: path.join(model.news.folder_path(), news_icon.replace(/\u0001/g, "."))
 				});
 			}
 		}
 	}
 	
 	model.news.loadNews();
-
+	
 	pahub.api.section.addSection("section-community", "COMMUNITY", path.join(folder, "community.png"), "sections", 10);
 	pahub.api.tab.addTab("section-community", "news", "NEWS", "", 10);
 	pahub.api.tab.addTab("section-community", "chat", "CHAT", "", 20);
@@ -99,6 +222,7 @@ function load_plugin_community(data, folder) {
 	setTimeout(function() {document.getElementById('iframe-chat').src = 'http://webchat.esper.net/?nick=&channels=planetaryannihilation'}, 1000);
 	setTimeout(function() {document.getElementById('iframe-forum').src = 'http://forums.uberent.com'}, 1000);
 	
+	model.content.getContentItems(false).subscribe(model.news.updateTags);
 }
 
 function unload_plugin_community(data) {
